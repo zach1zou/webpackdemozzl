a67e08aecb3d926e2ab47650acec63c2c44ff801
zach1zou <791522322@qq.com>
2022-06-27 20:36:02 +0800

    第一题全部写完

--- src/main.js
+++ src/main.js
@@ -3,3 +3,21 @@ import './tabs.js'
 
 import $ from 'jquery';
 $('#swiper').css('background-color', 'red')
+
+// import './styles/index.css';
+import './styles/index.less';
+// index.js
+import imgUrl from './assets/assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+// 引入字体图标文件
+import './assets/assets/fonts/iconfont.css'
+
+class App {
+  static a = 123
+}
+
+console.log(App.a)
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,69 @@ module.exports = {
       port: 3000, // 端口号
       open: true
     },
-    
+       module: { // loader 加载器 配置在这儿
+        rules: [ // loader的规则
+          {
+            test: /\.css$/, // 匹配所有的css文件
+            // loader 执行的顺序： use数组里从右向左运行
+            // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+            // 再用 style-loader 将样式, 把css插入到dom中
+            use: [ "style-loader", "css-loader"]
+          },
+       
+           // loader的规则
+    // ...省略其他
+    {
+    	test: /\.less$/, // 匹配执行类型的文件
+    	// 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+      // 执行的顺序 less-loader css-loader style-loader
+      // less-loader 先把less代码转换成css
+      // css-loader 再把css代码转换成webpack 可以识别的js代码
+      // style-loader 在把css代码插入到 dom中
+        use: [ "style-loader", "css-loader", 'less-loader']
+                },
+//                 {
+//   test: /\.(png|jpg|gif|jpeg)$/i,
+//   use: [
+//     {
+//       loader: 'url-loader', // 匹配文件, 尝试转base64字符串打包到js中
+//       // 配置limit, 超过8k, 不转, file-loader复制, 随机名, 输出文件
+//       options: {
+//         limit: 8 * 1024,
+//       },
+//     },
+//   ],
+// }
+{ // 图片文件的配置(仅适用于webpack5版本)
+    test: /\.(png|jpg|gif|jpeg)$/i,
+    type: 'asset', // 在导出一个 data URI 和发送一个单独的文件之间自动选择
+    // 如果你设置的是asset模式
+    // 以8KB大小区分图片文件
+    // 小于8KB的, 把图片文件转base64, 打包进js中
+    // 大于8KB的, 直接把图片文件输出到dist下
+      
+    // type: 'asset/resource' // 发送一个单独的文件并导出 URL
+    // type: 'asset/inline' // 导出一个资源的 data URI
+                },
+{ // webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+    test: /\.(eot|svg|ttf|woff|woff2)$/,
+    type: 'asset/resource',
+    generator: {
+    	filename: 'font-[name].[hash:6][ext]'
+    }
+               },
+                 {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env'] // 预设:转码规则(用bable开发环境本来预设的)
+              }
+          }
+      }
+  ]
+    }
+      
      
 }
